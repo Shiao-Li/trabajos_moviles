eba6d44529254fce05ada5ef10c2a452159e0277
Roberto Shiao <95731527+Shiao-Li@users.noreply.github.com>
2023-11-22 15:56:22 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAADPktnf11WzqV5ICqjIgbGUtY_WAAkcg",
+    authDomain: "b-35c0a.firebaseapp.com",
+    projectId: "b-35c0a",
+    storageBucket: "b-35c0a.appspot.com",
+    messagingSenderId: "958328402726",
+    appId: "1:958328402726:web:039fd652954ca6aea8c7e7",
+    measurementId: "G-8GL4WH2T3H"
+  },
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+//firebase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getStorage, provideStorage } from "@angular/fire/storage";
+
+import { 
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  EmailAuthProvider,
+  TwitterAuthProvider,
+  getAuth,
+  provideAuth
+ } from "@angular/fire/auth";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
